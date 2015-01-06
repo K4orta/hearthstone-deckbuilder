697c1282cb50f74c05154e4ba40632dafc805a47
ewong <ewong@blizzard.com>
2015-01-06 11:52:22 -0800

    port change for real this time

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get('/', function(req, res) {
 app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
-var server = app.listen(3000, function() {
+var server = app.listen(port, function() {
 	console.log("Started on port " + port);
 });
